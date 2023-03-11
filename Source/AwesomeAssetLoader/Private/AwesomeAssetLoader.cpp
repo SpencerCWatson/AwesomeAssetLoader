67834ddf6f845a4b9a820b23a6619fe972fc91f5
SpencerCWatson <spencercwatson@gmail.com>
2023-03-11 16:09:47 -0700

    Init

---
+++ Source/AwesomeAssetLoader/Private/AwesomeAssetLoader.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AwesomeAssetLoader.h"
+
+#define LOCTEXT_NAMESPACE "FAwesomeAssetLoaderModule"
+
+void FAwesomeAssetLoaderModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FAwesomeAssetLoaderModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FAwesomeAssetLoaderModule, AwesomeAssetLoader)
---
+++ Source/AwesomeAssetLoader/Private/AwesomeAssetManager.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "AwesomeAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AWESOMEASSETLOADER_API UAwesomeAssetManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override {};
+	virtual void Deinitialize() override {};
+};
